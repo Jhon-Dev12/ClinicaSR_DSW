16811a78cc399dae51125be82b7eb8881f7eb3bc
ivangonzalespurizaca <gonzalespurizacaoscarivan@gmail.com>
2025-12-11 13:36:45 -0500

    DALC corregido

--- ClinicaSR.DL.DALC/HorarioAtencionDALC.cs
+++ ClinicaSR.DL.DALC/HorarioAtencionDALC.cs
@@ -6,5 +6,21 @@ namespace ClinicaSR.DL.DALC
 {
     public class HorarioAtencionDALC
     {
+        public List<HorarioAtencionBE> listarHorariosPorMedico(int idMedico)
+        {
+
+        }
+        public HorarioAtencionBE editarHorario(HorarioBE horarioBE)
+        {
+
+        }
+        public HorarioAtencionBE registrarHorario(HorarioBE horarioBE)
+        {
+
+        }
+        public void eliminarHorarioPorId(int idHorario)
+        {
+
+        }
     }
 }
